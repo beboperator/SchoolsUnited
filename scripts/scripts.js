a246ba0359bc3aef992fec0e89cee955e5631d86
matt whalley <mttwhlly@icloud.com>
2019-02-05 03:04:53 -0500

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+// Inject Font Awesome link into head
+<script>
+head.ready(function() {
+console.log('js in bb is a go');  
+$('head').append('<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-6jHF7Z3XI3fF4XZixAuSu0gGKrXwoX/w3uFPxC56OtjChio7wtTGJWRW53Nhx6Ev" crossorigin="anonymous">')
+});
+</script>
+
+// TODO: Slide & up & cover hero (like the twitters do)
